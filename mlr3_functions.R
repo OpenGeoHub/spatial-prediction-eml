6924d73bf2b7aa580d50f03248b4888744674917
Leandro Parente <leal.parente@gmail.com>
2022-06-21 12:05:50 +0200

    train and test spliting

--- mlr3_functions.R
+++ mlr3_functions.R
@@ -13,126 +13,154 @@ library("igraph")
 
 ncores = parallel::detectCores()
 
+split_by_blocking = function(
+  df,
+  group.name,
+  pct = 0.5,
+  seed = 1989
+) {
+
+  set.seed(seed)
+  uniq.group = unique(df[,group.name])
+  train.groups = uniq.group[ sample(length(uniq.group), round(length(uniq.group) * pct) )]
+
+  return(list(
+    train = filter(df, df[,group.name] %in% train.groups),
+    test = filter(df, !df[,group.name] %in% train.groups)
+  ))
+
+}
+
 train_mlr3_model = function(
   in.data, 
   target, 
   learner, 
-  search_space, 
   measure,
-  terminator = trm("evals", n_evals = 10),
-  resampling = rsmp("cv", folds = 5),
-  tuner = tnr("random_search"),
-  fselector = fs("random_search"),
-  afs.learner = lrn('classif.ranger', num.trees=40, importance = "impurity", num.threads = ncores),
-  subsample.pct = 0.1, # Hyperparameter tunning
+  tnr.search_space, 
+  fs.selector = fs("random_search"),
+  fs.resampling = rsmp("cv", folds = 5),
+  fs.terminator = trm("evals", n_evals = 10),
+  fs.learner = lrn('classif.ranger', num.trees=40, importance = "impurity", num.threads = ncores),
+  tnr.subsample.pct = 0.1,
+  tnr.terminator = trm("evals", n_evals = 10),
+  tnr.resampling = rsmp("cv", folds = 5),
+  tnr.tuner = tnr("random_search"),
+  val.resampling = rsmp("cv", folds = 5),
   mlr_log_level = 'info',
   group.blocking = '',
+  seed = 1989,
   verbose = TRUE
 ) 
 {
   
   task_type <- learner$task_type
   id = paste0(learner$id, '_', target)
-  sample_size = as.integer(nrow(in.data) * subsample.pct)
+  tnr.subsample.size = as.integer(nrow(in.data) * tnr.subsample.pct)
   
   lgr::get_logger("mlr3")$set_threshold(mlr_log_level)
   lgr::get_logger("bbotk")$set_threshold(mlr_log_level)
   
-  sample.rows <- sample(nrow(in.data), sample_size)
+  tnr.subsample.rows <- sample(nrow(in.data), tnr.subsample.size)
   
   ########################################################
   #### Feature selection
   ########################################################
   
   if (task_type == 'regr') {
-    task.afs <- TaskRegr$new(target, in.data, target = target)
+    fs.task <- TaskRegr$new(target, in.data, target = target)
   } else {
-    task.afs <- TaskClassif$new(target, in.data, target = target, positive ="1")
+    fs.task <- TaskClassif$new(target, in.data, target = target, positive ="1")
   }
   
   if (group.blocking != '') {
-    task.afs$set_col_roles(group.blocking, roles = "group")
+    fs.task$set_col_roles(group.blocking, roles = "group")
   }
   
-  if (verbose) print(paste0('Finding the most important covariates using ', nrow(in.data), ' samples'))
-  
+  if (verbose) print(paste0(Sys.time(), ' - ', 'Finding the most important covariates using ', nrow(in.data), ' samples'))
+  set.seed(seed)
+
   afs = AutoFSelector$new(
-    learner = afs.learner,
-    resampling = resampling, 
+    learner = fs.learner,
+    resampling = fs.resampling, 
     measure = measure,
-    terminator = terminator,
-    fselector = fselector,
+    terminator = fs.terminator,
+    fselector = fs.selector,
     store_fselect_instance = TRUE
   )
   
   invisible(capture.output({
-    afs$train(task.afs)
+    afs$train(fs.task)
   }))
-  #return(afs.result)
-  afs.result = afs$fselect_result$features[[1]]
+  #return(fs.result)
+  fs.result = afs$fselect_result$features[[1]]
   
   ########################################################
   #### Hyper-parameter optimization
   ########################################################
   
-  sub.names = c(afs.result, c(target, group.blocking))
+  sub.names = c(fs.result, c(target, group.blocking))
   
-  if (verbose) print(paste0(length(afs.result), ' covariates selected from ', (ncol(in.data) - 2)))
+  if (verbose) print(paste0(Sys.time(), ' - ', length(fs.result), ' covariates selected from ', (ncol(in.data) - 2)))
   
   # The attempt to use TaskSupervised does not work
   if (task_type == 'regr') {
-    task.tnr <- TaskRegr$new(target, in.data[sample.rows, sub.names], target = target)
-    task.rsp <- TaskRegr$new(target, in.data[,sub.names], target = target)
-    task.bst <- TaskRegr$new(target, in.data[,sub.names], target = target)
+    tnr.task <- TaskRegr$new(target, in.data[tnr.subsample.rows, sub.names], target = target)
+    val.task <- TaskRegr$new(target, in.data[,sub.names], target = target)
+    fin.task <- TaskRegr$new(target, in.data[,sub.names], target = target)
   } else {
-    task.tnr <- TaskClassif$new(target, in.data[sample.rows, sub.names], target = target, positive ="1")
-    task.rsp <- TaskClassif$new(target, in.data[,sub.names], target = target, positive ="1")
-    task.bst <- TaskClassif$new(target, in.data[,sub.names], target = target, positive ="1")
+    tnr.task <- TaskClassif$new(target, in.data[tnr.subsample.rows, sub.names], target = target, positive ="1")
+    val.task <- TaskClassif$new(target, in.data[,sub.names], target = target, positive ="1")
+    fin.task <- TaskClassif$new(target, in.data[,sub.names], target = target, positive ="1")
   }
   
   if (group.blocking != '') {
-    task.tnr$set_col_roles(group.blocking, roles = "group")
-    task.rsp$set_col_roles(group.blocking, roles = "group")
-    task.bst$set_col_roles(group.blocking, roles = "group")
+    tnr.task$set_col_roles(group.blocking, roles = "group")
+    val.task$set_col_roles(group.blocking, roles = "group")
+    fin.task$set_col_roles(group.blocking, roles = "group")
   }
   
-  if (verbose) print(paste0('Tunning hyperparameters using ', length(sample.rows), ' samples'))
-  
+  if (verbose) print(paste0(Sys.time(), ' - ', 'Tunning hyperparameters using ', length(tnr.subsample.rows), ' samples'))
+  set.seed(seed)
+
   instance = TuningInstanceSingleCrit$new(
-    task = task.tnr,
+    task = tnr.task,
     learner = learner$clone(),
-    resampling = resampling,
+    resampling = tnr.resampling,
     measure = measure,
-    search_space = search_space,
-    terminator = terminator
+    search_space = tnr.search_space,
+    terminator = tnr.terminator
   )
   
   invisible(capture.output({
-    tuner$optimize(instance)
+    tnr.tuner$optimize(instance)
   }))
   
-  if (verbose) print(paste0('Running model evaluation'))
-  best.learner = learner$clone()
-  best.learner$param_set$values = instance$result$learner_param_vals[[1]]
-  
+  if (verbose) print(paste0(Sys.time(), ' - ', 'Validating model'))
+  set.seed(seed)
+
+  bst.learner = learner$clone()
+  bst.learner$param_set$values = instance$result$learner_param_vals[[1]]
+
   invisible(capture.output({
     cv_pedictions = resample(
-      task = task.rsp,
-      learner = best.learner,
-      resampling = resampling,
+      task = val.task,
+      learner = bst.learner,
+      resampling = val.resampling,
     )$prediction()
   }))
   
-  if (verbose) print('Fiting the final model')
-  best.learner = learner$clone()
-  best.learner$param_set$values = instance$result$learner_param_vals[[1]]
+  if (verbose) print(paste0(Sys.time(), ' - ', 'Fiting the final model'))
+  set.seed(seed)
+
+  bst.learner = learner$clone()
+  bst.learner$param_set$values = instance$result$learner_param_vals[[1]]
   invisible(capture.output({
-    best.learner$train(task.bst) 
+    bst.learner$train(fin.task) 
   }))
   
   return(list(
-    learner = best.learner,
-    fs_result = afs.result,
+    learner = bst.learner,
+    fs_result = fs.result,
     cv_pedictions = as.data.table(cv_pedictions)
   ))
   
--- mlr3_modeling.R
+++ mlr3_modeling.R
@@ -1,26 +1,61 @@
-setwd('/home/leandro/Code/spatial-prediction-eml/')
+#######################################################
+###### r environment preparation
+#######################################################
+
+#setwd('/home/leandro/Code/spatial-prediction-eml/')
+
+ls <- c("dplyr", "mlr3", "mlr3learners", "mlr3pipelines", "mlr3filters", 
+        "mlr3extralearners", "mlr3fselect", "mlr3misc", "paradox", 
+        "mlr3tuning", "paradox", "igraph")
+new.packages <- ls[!(ls %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(ls, require, character.only = TRUE)
+
 source('./mlr3_functions.R')
 
 #######################################################
 ###### Input data preparation
 #######################################################
 
-rm.all = readRDS("./input/regmatrix_aedes_1km.rds")
-dim(rm.all)
+all.data = readRDS("./input/regmatrix_aedes_1km.rds")
 
-group.name = 'ID'
 target.name = 'occurrence'
-rm.all[target.name] = as.factor(ifelse(rm.all$individualCount>0, 1, 0))
-rm.names = names(rm.all)
+group.name = 'ID'
+seed = 1989
+
+all.data[target.name] = as.factor(ifelse(all.data$individualCount>0, 1, 0))
+print(dim(all.data))
+
+split = split_by_blocking(all.data, group.name, pct = 0.5, seed = seed)
+
+train.data = split$train
+print(dim(train.data))
+
+test.data =  split$test
+print(dim(test.data))
+
+# Checking if the groups
+# are complementary
+#print(paste0(
+#  length(unique(train.data$ID)), ' ',
+#  length(unique(test.data$ID)), ' ',
+#  length(unique(all.data$ID))
+#))
+
+v1km.names = names(train.data)
 
 feat.names = c(
-  rm.names[grepl('*_1km_*', rm.names)],  
+  v1km.names[grepl('*_1km_*', v1km.names)],  
   c("CRP", "FOR", "HFP", "LST", "N02", "N08", "NLT", "PPD", "PRE", "SNW", "T02", "T08")
 )
 
 mdl.names = c(target.name, group.name)
-rm.data = na.omit(rm.all[c(feat.names, mdl.names)])
-rm.data = rm.data[sample(nrow(rm.data), 1000),]
+train.data = na.omit(train.data[c(feat.names, mdl.names)])
+
+set.seed(seed)
+train.rows = sample(nrow(train.data), 3000)
+train.data = train.data[train.rows,]
+print(dim(train.data))
 
 ncores = parallel::detectCores()
 
@@ -28,7 +63,7 @@ ncores = parallel::detectCores()
 ###### mlr3Pipeline
 #######################################################
 
-eml.graph = gunion(
+stacked.graph = gunion(
   list(
     po("learner_cv", lrn("classif.glmnet", predict_type = "prob")),
     po("learner_cv", lrn("classif.xgboost", predict_type = "prob", nthread = ncores)),
@@ -37,15 +72,18 @@ eml.graph = gunion(
   po("featureunion") %>>%
   lrn("classif.log_reg", predict_type = "prob")
 
-eml.graph$plot(html = FALSE)
+stacked.graph$plot(html = FALSE)
+stacked.learner = as_learner(stacked.graph)
 
-eml.learner = as_learner(eml.graph)
+# Single learner example
+# to be on train_mlr3_model method
+# single.learner = lrn('classif.ranger', predict_type = "prob", importance = "impurity", num.threads = ncores)
 
 #######################################################
 ###### Feature selection learner
 #######################################################
 
-afs.learner = lrn('classif.ranger', num.trees=40, importance = "impurity",  predict_type = "prob",  num.threads = ncores)
+fs.learner = lrn('classif.ranger', num.trees=60,  predict_type = "prob",  num.threads = ncores)
 
 #######################################################
 ###### Hyper-parameter search space
@@ -58,28 +96,53 @@ stacked.search_space = ps(
   classif.xgboost.eta = p_dbl(0.2, 0.4),
   classif.xgboost.subsample = p_dbl(0.9, 1),
   classif.xgboost.min_child_weight = p_int(1, 4),
-  classif.xgboost.colsample_bytree = p_dbl(0.5, 0.6)
+  classif.xgboost.colsample_bytree = p_dbl(0.5, 0.6),
+  classif.glmnet.alpha = p_dbl(0, 1),
+  classif.glmnet.lambda = p_dbl(50, 150)
 )
+
+# Search space for a single learner
+# to be on train_mlr3_model method
+#single.search_space = ps(
+#  num.trees =  p_int(lower = 30, upper = 60)
+#)
 
 #######################################################
 ###### Modeling execution
 #######################################################
 
+fs.selector = fs(
+  "genetic_search", 
+  popSize = length(feat.names), 
+  elitism = round(length(feat.names) * 0.8), 
+  mutationChance = 0.1,
+  verbose = TRUE
+)
+
 result = train_mlr3_model(
-  in.data = rm.data,
+  in.data = train.data,
   target = target.name,
-  learner = eml.learner,
-  search_space = stacked.search_space,
-  group.blocking = group.name,
+  learner = stacked.learner,
   measure=msr('classif.logloss'),
-  terminator = trm("evals", n_evals = 20),
-  resampling = rsmp("cv", folds = 5),
-  tuner = tnr("random_search"),
-  fselector = fs("genetic_search"),
-  afs.learner = lrn('classif.ranger', num.trees=40, importance = "impurity",  predict_type = "prob"),
-  mlr_log_level = 'fatal',
+  fs.selector = fs.selector,
+  fs.selector = fs("genetic_search", popSize = length(feat.names), elitism = round(length(feat.names) * 0.8), mutationChance = 0.1),
+  fs.resampling = rsmp("cv", folds = 5),
+  fs.terminator = trm("run_time", secs = 120),
+  fs.learner = fs.learner,
+  
+  tnr.search_space = stacked.search_space,
+  tnr.subsample.pct = 0.10,
+  tnr.terminator = trm("evals", n_evals = 10),
+  tnr.resampling = rsmp("cv", folds = 5),
+  tnr.tuner = tnr("random_search"),
+  
+  val.resampling = rsmp("cv", folds = 5),
+  
+  group.blocking = group.name,
+  
+  mlr_log_level = 'fatal', # fatal, error, warn, info, debug, trace
   verbose = TRUE,
-  subsample.pct = 0.10
+  seed = seed
 )
 
 #######################################################
@@ -97,6 +160,21 @@ mlr3measures::confusion_matrix(result$cv_pedictions$truth, result$cv_pedictions$
 
 rf.importance = result$learner$model$classif.ranger$model$variable.importance
 rf.var.imp = as.data.frame(rf.importance, col.names=list('importance'))
+rf.var.imp[order(-rf.var.imp$rf.importance),, drop=FALSE]
+
+#######################################################
+###### Prediction / Generalization
+#######################################################
+
+new.data = na.omit(test.data[,c(feat.names,target.name)])
+print(dim(new.data))
+
+test.pred.prob = predict(result$learner, new.data[,feat.names], predict_type = 'prob')[,'1']
+test.pred.truth = as.numeric(as.character(new.data[, 'occurrence']))
+
+verification::roc.plot(test.pred.truth, test.pred.prob, show.thres=F, plot.thres=T)
+verification::roc.area(test.pred.truth, test.pred.prob)
+mlr3measures::confusion_matrix(result$cv_pedictions$truth, result$cv_pedictions$response, positive='1')
 
 #######################################################
 ###### Saving output
